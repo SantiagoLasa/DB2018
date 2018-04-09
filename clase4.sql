657c147dd4594db338b204e46b1fbc2b279bcc9b
Sr. Lasa Santiago <santi.lasa99@gmail.com>
2018-04-09 15:26:15 -0300

    Update clase4.sql

--- clase4.sql
+++ clase4.sql
@@ -1,29 +1,35 @@
 1)
+
 SELECT f.title, f.special_features 
 FROM film f 
 WHERE f.rating LIKE "PG-13"
 
 2)
+
 SELECT f1.title, f2.title, f1.`length` 
   FROM film f1, film f2
 WHERE f1.`length` <> f2.`length`
 
 3)
+
 SELECT f.title, f.rental_rate, f.replacement_cost
 	FROM film f 
 where f.replacement_cost BETWEEN 20 and 24
 
 4)
+
 SELECT f.title, c.name, f.rating, f.special_features
  FROM film f, film_category fc,  category c 
 WHERE f.special_features LIKE "Behind the scenes";
 
 5)
+
 SELECT a.first_name, a.last_name , f.title
 FROM actor a, film_d the manager first/last name of this store (actor fa, film f
 WHERE f.title LIKE "ZOOLANDER FICTION";
 
 6)
+
 SELECT  ad.address, co.country, ci.city, s.store_id
 FROM country co , city ci , address ad, store s
 WHERE s.store_id=1
